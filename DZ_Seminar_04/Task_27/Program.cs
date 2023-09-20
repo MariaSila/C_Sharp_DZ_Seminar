a9a7cddd185bf6fb7a8ac547e18f94098ed214d2
Maria <dmn02@mail.ru>
2023-09-20 15:26:50 +0500

    ДЗ к Семинару 4 Добавлено решение Задачи 27

---
+++ DZ_Seminar_04/Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 27: Напишите программу, которая принимает
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumDigit(int number)
+{
+    int sum = 0;
+    int digit = 0;
+    while (number > 0)
+    {
+        digit = number % 10;        // 7298 % 10 = 8
+        sum += digit;               // 0+8=8
+        number = number / 10;       // 7298 / 10 = 729
+    }
+    return sum;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+System.Console.WriteLine($"Сумма цифр числа {number} = {SumDigit(number)}");
