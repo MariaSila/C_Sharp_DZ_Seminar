d11b144e6d54e2e3dde0c7e2e57dc00c34bd79ec
Maria <dmn02@mail.ru>
2023-09-20 11:37:50 +0500

    ДЗ Автопроверка к Семинару 2 Решение задач 13,15

---
+++ DZ_Seminar_02/Task_13/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+123456789 -> 3
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+int thirdDigit = -1;
+while (number >= 1000)
+{
+    number = number / 10;
+} 
+int result = number; 
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+} 
+else
+{
+    thirdDigit = result % 10;
+    Console.WriteLine(thirdDigit);
+}   
---
+++ DZ_Seminar_02/Task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+System.Console.Write("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine()!);
+if (day > 5 & day < 8)
+{
+    System.Console.WriteLine("Да");
+}
+else if (day > 0 & day < 6)
+{
+    System.Console.WriteLine("Нет");
+}
+else
+{
+    System.Console.WriteLine("Ошибка! Введите число от 1 до 7");
+}
