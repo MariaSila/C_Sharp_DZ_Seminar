5f0cdde5eb84e1cf8b09a606185a82e4759f0aa2
Maria <dmn02@mail.ru>
2023-09-20 14:58:18 +0500

    ДЗ к Семинару 4 Добавлено решение Задачи 25

---
+++ DZ_Seminar_04/Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+int NumberDegree(int numberA, int numberB)
+{
+  int result = 1;
+  int count = 1;
+  while (count <= numberB)
+  {
+    result *= numberA;
+    count ++;
+  }
+  return result;
+}
+
+System.Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine()!);
+System.Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine()!);
+System.Console.WriteLine($"Число {numberA} в {numberB} степени = {NumberDegree(numberA, numberB)}");
