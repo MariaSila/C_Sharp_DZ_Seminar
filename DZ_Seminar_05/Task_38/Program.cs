9b405aab09818b22f8caca953869a597e3b746b5
Maria <dmn02@mail.ru>
2023-09-23 12:25:18 +0500

    ДЗ к Семинару 5 Добавлено решение Задачи 38

---
+++ DZ_Seminar_05/Task_38/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+
+double[] GetArray (int length, int minValue, int maxValue)
+{
+    double[] array = new double [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue) + new Random().NextDouble();
+    }
+    return array;
+}
+
+double MaxNumber(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)   
+    {
+        if (max < array[i])
+            max = array[i];
+    }
+    return max;
+}
+
+double MinNumber (double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+            min = array[i]; 
+    }
+    return min;
+}
+
+void PrintArray (double[] array)
+{
+    System.Console.Write("Массив: ");
+    foreach (double item in array)
+    {
+        System.Console.Write($"{item:f} ");
+    }
+    System.Console.WriteLine();
+}
+
+double[] array = GetArray(length:10, minValue:0, maxValue:10);
+PrintArray(array);
+System.Console.WriteLine($"Max элемент массива: {MaxNumber(array):f}");
+System.Console.WriteLine($"Min элемент массива: {MinNumber(array):f}");
+System.Console.WriteLine($"Max - Min = {MaxNumber(array) - MinNumber(array):f}");
