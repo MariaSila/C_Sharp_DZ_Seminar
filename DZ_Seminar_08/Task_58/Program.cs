5d70333406400eeca4ec94e954cb09590e39bea8
Maria <dmn02@mail.ru>
2023-10-04 08:26:39 +0500

    ДЗ к Семинару 8 Внесено улучшение в задачу 58

--- DZ_Seminar_08/Task_58/Program.cs
+++ DZ_Seminar_08/Task_58/Program.cs
@@ -23,10 +23,17 @@ int[,] GetArray(int n, int m, int minValue = 1, int maxValue = 9)
 
 void PrintArray(int[,] array)
 {
+    int error = -1;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
+            if (array[0, 0] == error)
+            {
+                System.Console.WriteLine("Ошибка! Количество столбцов первой матрицы должно равняться количеству строк второй матрицы.");
+                System.Console.WriteLine();
+                return;
+            }
             System.Console.Write(array[i, j] + "\t");
         }
         System.Console.WriteLine();
@@ -38,8 +45,8 @@ int[,] ProductOfMatrix(int[,] array, int[,] multiplier)
 {
     int[,] product = new int[array.GetLength(0), multiplier.GetLength(1)];
     int[,] error = {
-        {0, 0}, 
-        {0, 0},
+        {-1, -1}, 
+        {-1, -1},
     };
     if (array.GetLength(1) != multiplier.GetLength(0))
     {
@@ -61,10 +68,13 @@ int[,] ProductOfMatrix(int[,] array, int[,] multiplier)
     return product;
 }
 
-int[,] matrix_1 = GetArray(4, 4);
+int[,] matrix_1 = GetArray(4, 3);
+System.Console.WriteLine("Первая матрица:");
 PrintArray(matrix_1);
 int[,] matrix_2 = GetArray(3, 2);
+System.Console.WriteLine("Вторая матрица:");
 PrintArray(matrix_2);
 int[,] product_matrix = ProductOfMatrix(matrix_1, matrix_2);
+System.Console.WriteLine("Произведение двух матриц:");
 PrintArray(product_matrix);
 
